9f10099aa9941cfea99e337772da0db0e7655fde
Manoj Sharma <imanojsharma07@gmail.com>
2025-06-23 13:48:30 +0530

    Jupyter EDA and Model training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys 
-import logging 
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -25,9 +25,9 @@ class CustomException(Exception):
 Testing exception.py by running using main function
 '''   
     
-# if __name__ == "__main__":
-#     try:
-#         a = 1/0
-#     except Exception as e:
-#         logging.info("Divide by ZERO !!!")
-#         raise CustomException(e, sys)
+if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by ZERO !!!")
+        raise CustomException(e, sys)
